2bdc2e82a19d224aa518c9da9cb84f4121fad7b3
Christie Bahlai <cbahlai@msu.edu>
2017-01-16 15:56:36 -0500

    Create project, outline script

---
+++ Functional_response_analysis.R
@@ -0,0 +1,11 @@
+# Functional response of four predator species to varied densities of stink bug eggs
+
+#bring data in
+
+#create subsets for each species
+
+#fit to Junilano 2001 model to determine type of functional response for each species
+
+#fit curve of relevant type to determine consumption rate as a function of egg density
+
+#create plots to illustrate
