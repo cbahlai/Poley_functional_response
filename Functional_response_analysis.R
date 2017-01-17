7bcb5c28691c2f9215b791c918036630c17d946b
Christie Bahlai <cbahlai@msu.edu>
2017-01-17 13:32:58 -0500

    More analysis planning

--- Functional_response_analysis.R
+++ Functional_response_analysis.R
@@ -4,7 +4,21 @@
 
 #create subsets for each species
 
-#fit to Junilano 2001 model to determine type of functional response for each species
+#we're going to use the approach of a two stage analysis. Basically, step one is determine
+#what the response is, then step 2 is fit the data to the appropriate response and build a plot
+
+#step 1- find out the fit
+
+# fit to Juilano 2001 model to determine type of functional response for each species
+# juliano created an nth order logistic equation to model the relationship between the number
+# of prey consumed and the initial prey density. It should be sufficient to take this model 
+# just to cubic form, and if the fit is poor from there, reduce the order.
+# The way this fit is used, if the linear coefficient (P1) is negative
+# then we consider the functional response to be of type II, but if it's positive
+# then the functional response is type III. If P1 is not significant, reduce the order 
+# (ie set P3 to zero) and refit.
+
+
 
 #fit curve of relevant type to determine consumption rate as a function of egg density
 
