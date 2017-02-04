3b516bc14064dcec7e72ade10006096bf9164776
Christie Bahlai <cbahlai@msu.edu>
2017-02-04 10:59:41 -0500

    Update grasshopper and katydid analysis, create figures

--- Functional_response_analysis.R
+++ Functional_response_analysis.R
@@ -321,7 +321,7 @@ AIC(orius.linear)
 
 #the AIC also indicates that this is not a great model, but at least there's a significant slope
 #so how I'd interpret this is there is density dependance in the response, but it can't be defined
-#because either the data are too variable, or an insufficient rance of starting values are covered
+#because either the data are too variable, or an insufficient range of starting values are covered
 
 
 #calculate mean and SEM for each treatment
@@ -330,7 +330,8 @@ Orius.summary<-ddply(Orius, c("eggs_start"), summarise,
                        sem=sd(eggs_eaten)/sqrt(n))
 
 #create objects describing functions for plotting our fit- we'll use the linear one because it's the simplest 
-#model, and there's no real support for a curvilinear model
+#model, and there's no real support for a curvilinear model, even though the random predator equation had the 
+#lowest AIC
 
 b<-summary(orius.linear)$coefficients[1,1]
 m<-summary(orius.linear)$coefficients[2,1]
@@ -350,6 +351,12 @@ Orius.plot<-ggplot(Orius.summary, aes(x=eggs_start, y=mean_eggs_eaten))+
   theme_bw()+ theme(legend.key=element_rect(colour=NA))
 
 Orius.plot
+
+#save to pdf
+pdf("Orius_fig.pdf", height=4, width=5)
+Orius.plot
+dev.off()
+
 
 ##Next up Grasshopper!!
 Grasshopper<-read.csv(file="Grasshopper.csv", header=T)
@@ -395,9 +402,56 @@ Grasshopper.F.fit<-nls(Pconsumed~
 
 summary(Grasshopper.F.fit)
 
-##Skip Grasshopper Female for now, not sure how to continue, no sig negative coefficient
+##We have a negative, but non-significant value for P1, as we had with orius- 
+# this suggests there may be a type II functional response
+# but data are too variable or do not capture a sufficient range of the relationship to see this clearly.
 
-#let's look at Grasshopper males
+#we can attempt to fit Random Predator and Holling's Disc but I think we're just going to end up with a linear model
+#like orius again
+
+
+###
+# step 2- fit the functional response
+
+#Whatever the model, we're trying to find the values for a, attack rate, and Th, handling time
+#so we need starting values
+
+
+a<-1
+Th<-0
+
+#fit the random predator equation
+Grasshopper.F.random<-nls(eggs_eaten~eggs_start*(1-exp(a*(Th*eggs_eaten-1))), 
+                  start=list(a=a,Th=Th),
+                  data=Grasshopper.F)
+
+summary(Grasshopper.F.random)
+AIC(Grasshopper.F.random)
+#fit the Holling's disc equation
+
+Grasshopper.F.holling<-nls(eggs_eaten~eggs_start*a /(1+a*Th*eggs_start), 
+                   start=list(a=a,Th=Th),
+                   data=Grasshopper.F)
+
+summary(Grasshopper.F.holling)
+AIC(Grasshopper.F.holling)
+
+
+#again, we have better performance of the Random predator equation but no significant parameters in either. 
+#I think we just need to use a straight line here
+
+#let's fit a linear model, for a type I response
+
+Grasshopper.F.linear<-lm(eggs_eaten~eggs_start, data=Grasshopper.F)
+
+summary(Grasshopper.F.linear)
+AIC(Grasshopper.F.linear)
+
+#the AIC also indicates that this is not a great model, but again, at least there's a significant slope
+#so how I'd interpret this is there is density dependance in the response, but it can't be defined
+#because either the data are too variable, or an insufficient range of starting values are covered
+
+#let's  do the same for Grasshopper males
 
 Grasshopper.M.fit<-nls(Pconsumed~
                      exp(P0+
@@ -428,6 +482,70 @@ Grasshopper.M.fit<-nls(Pconsumed~
 summary(Grasshopper.M.fit)
 
 ##Removing the higher order changes P1 from negative to positive and neither are significant
+## this means there's practically no evidence for Type II response, and I suspect we don't have sufficient range 
+#of data even if we do have a type III response, so I think we should go straight to modelling the males as a linear relationship
+
+#let's fit a linear model, for a type I response
+
+Grasshopper.M.linear<-lm(eggs_eaten~eggs_start, data=Grasshopper.M)
+
+summary(Grasshopper.M.linear)
+AIC(Grasshopper.M.linear)
+
+# we don't even have a significant slope in this case, but it may still be worthwhile to plot the line on our graph
+
+#choose colors for each sex
+# colors from a qualitative pallate from colorbrewer.org, chosen for colorblind friendliness
+female<-"#d95f02"
+male<-"#7570b3"
+
+#calculate mean and SEM for each treatment
+Grasshopper.summary<-ddply(Grasshopper, c("predator_sex", "eggs_start"), summarise,
+                       mean_eggs_eaten=mean(eggs_eaten), n=length(eggs_eaten),
+                       sem=sd(eggs_eaten)/sqrt(n))
+
+#create objects describing functions for plotting our fits
+#females
+
+b<-summary(Grasshopper.F.linear)$coefficients[1,1]
+m<-summary(Grasshopper.F.linear)$coefficients[2,1]
+Grasshopper.F.func.linear<-function(x)  x*m+b #y=mx+b, the linear equation general formula
+
+#males
+bm<-summary(Grasshopper.M.linear)$coefficients[1,1]
+mm<-summary(Grasshopper.M.linear)$coefficients[2,1]
+Grasshopper.M.func.linear<-function(x)  x*mm+bm #y=mx+b, the linear equation general formula
+
+# The errorbars overlapped, so use position_dodge to move them horizontally
+pd <- position_dodge(3) # move them 3 units to the left and right
+
+Grasshopper.plot<-ggplot(Grasshopper.summary, aes(x=eggs_start, y=mean_eggs_eaten, 
+                                          color=predator_sex, shape=predator_sex))+
+  scale_color_manual(values=c(female, male), name="Predator sex")+
+  scale_shape_manual(values=c(16,17), name="Predator sex")+
+  stat_function(fun=Grasshopper.F.func.linear, colour=female, size=1, linetype="dashed")+
+  stat_function(fun=Grasshopper.M.func.linear, colour=male, size=1, linetype="dotted")+ 
+  geom_errorbar(aes(ymin=mean_eggs_eaten-sem, ymax=mean_eggs_eaten+sem, color=predator_sex), 
+                position=pd, color="black", width=5, size=0.5, show.legend=FALSE) +
+  xlim(0, 150)+ylim(0,60)+
+  xlab("Starting egg density")+ylab("Eggs eaten")+
+  geom_point(position=pd, size=4, show.legend=TRUE)+
+  theme_bw()+ theme(legend.key=element_rect(colour=NA))
+
+
+Grasshopper.plot
+
+
+#save to pdf
+pdf("Grasshopper_fig.pdf", height=4, width=5)
+Grasshopper.plot
+dev.off()
+
+
+
+
+
+
 ##Instead of moving to step 2 for grasshopper, I will do step 1 for Katydid
 
 ###
@@ -454,8 +572,14 @@ Katydid.fit<-nls(Pconsumed~
                    start=list(P0=P0, P1=P1, P2=P2, P3=P3),
                    data=Katydid)
 
-summary(Katydid.fit)
-###Error when I run this...Katydid.fit not found
+
+## so this fit is failing entirely because the data is not covering the curve, so it's failing to converge
+# (essentially that means that bcause there's no data at places where the model predicts the data to curve, the computer
+#is whipping the line all around to find a solution but can't, because there's no data to fit the curve to, so many solutions 
+# have equal fit)- I fiddled around with the defaults on the number of iterations and the tolerances for what it considers a
+#solution, but it's really the data coverage that's the issue here.
+
+#our only possibility here is to reduce the model- make it less curvy, to see if the computer can find a best fit them
 #Let's refit without the 3rd order polynomial
 Katydid.fit<-nls(Pconsumed~
                          exp(P0+
@@ -468,4 +592,80 @@ Katydid.fit<-nls(Pconsumed~
                        data=Katydid)
 
 summary(Katydid.fit)
-##Same error when I run the lower order model...Katydid.fit not found
+# We're now getting a nonsignificant, but negative P1- so some possible weak evidence for a Type II response. 
+# We can proceed as we did with Orius- test to see if Random Predator or Holling see anything, and if not, model it as a line
+
+
+###
+# step 2- fit the functional response
+
+#Whatever the model, we're trying to find the values for a, attack rate, and Th, handling time
+#so we need starting values
+
+
+a<-1
+Th<-0
+
+#fit the random predator equation
+Katydid.random<-nls(eggs_eaten~eggs_start*(1-exp(a*(Th*eggs_eaten-1))), 
+                  start=list(a=a,Th=Th),
+                  data=Katydid)
+#convergence problems again, this time for the random predator equation. I suspect these data just don't fit a Type II 
+#curve very well, and we're just getting more evidence of that.
+
+
+#fit the Holling's disc equation
+
+Katydid.holling<-nls(eggs_eaten~eggs_start*a /(1+a*Th*eggs_start), 
+                   start=list(a=a,Th=Th),
+                   data=Katydid)
+
+summary(Katydid.holling)
+AIC(Katydid.holling)
+
+#Holling's disc has no significant parameters- and it's, in fact, saying negative handling 
+# time (although non-significant) so that's a pretty strong indication to me of a poor fit!
+
+#As before, I think a linear response is the best we can do
+
+#let's fit a linear model, for a type I response
+
+Katydid.linear<-lm(eggs_eaten~eggs_start, data=Katydid)
+
+summary(Katydid.linear)
+AIC(Katydid.linear)
+
+# No significant slope
+#so very little trend overall
+
+
+#calculate mean and SEM for each treatment
+Katydid.summary<-ddply(Katydid, c("eggs_start"), summarise,
+                     mean_eggs_eaten=mean(eggs_eaten), n=length(eggs_eaten),
+                     sem=sd(eggs_eaten)/sqrt(n))
+
+#create objects describing functions for plotting our fit- we'll use the linear one because it's the simplest 
+#model, and there's no real support for a curvilinear model, even though it's also not significant
+
+b<-summary(Katydid.linear)$coefficients[1,1]
+m<-summary(Katydid.linear)$coefficients[2,1]
+Katydid.func.linear<-function(x)  x*m+b #y=mx+b, the linear equation general formula
+
+# The errorbars overlapped, so use position_dodge to move them horizontally
+pd <- position_dodge(3) # move them .05 to the left and right
+
+Katydid.plot<-ggplot(Katydid.summary, aes(x=eggs_start, y=mean_eggs_eaten))+ 
+  stat_function(fun=Katydid.func.linear, size=1, linetype="dashed")+
+  geom_errorbar(aes(ymin=mean_eggs_eaten-sem, ymax=mean_eggs_eaten+sem), 
+                position=pd, color="black", width=3, size=0.75, show.legend=FALSE) +
+  xlim(0, 150)+ylim(0,13)+
+  xlab("Starting egg density")+ylab("Eggs eaten")+
+  geom_point(position=pd, size=5, show.legend=TRUE)+
+  theme_bw()+ theme(legend.key=element_rect(colour=NA))
+
+Katydid.plot
+
+#save to pdf
+pdf("Katydid_fig.pdf", height=4, width=5)
+Katydid.plot
+dev.off()
